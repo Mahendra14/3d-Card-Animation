e52701544986bfe9ffd046fa6c5b4c061ea35d75
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-11-01 11:09:54 +0530

    added just var to js

---
+++ app.js
@@ -0,0 +1,9 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
